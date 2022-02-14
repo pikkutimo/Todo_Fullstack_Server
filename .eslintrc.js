eb191d145cea5e1b98bd71161622c9bdb01c7c4c
Timo Vilen <timo.a.vilen@tuni.fi>
2022-02-14 08:56:01 +0200

    Current

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'eqeqeq': 'error',
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
--- models/todo.js
+++ models/todo.js
@@ -5,25 +5,32 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('Connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB: ', error.message)
-    })
+  .then(result => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB: ', error.message)
+  })
 
 const todoSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean,
 })
 
 todoSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Todo', todoSchema)
